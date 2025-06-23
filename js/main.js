ce97c1a7c2b6ab74df4e4b4b3f2fc1772467693f
Aayush <aayushpokharel03@gmail.com>
2025-06-23 13:33:21 +0545

    main.js

--- js/main.js
+++ js/main.js
@@ -3,6 +3,17 @@ document.addEventListener('DOMContentLoaded',function(){
     const auth = new AuthManager(storage);
     const admin = new DashboardManager(storage,auth);
     const reviews = new ReviewManager(storage,auth);
-    
+
+    const loginModal = document.getElementById('login-modal');
+    const registerModal = document.getElementById('register-modal');
+    const adminModal = document.getElementById('admin-modal');
+    const userDashboard = document.getElementById('user-dashboard');
+    const adminDashboard = document.getElementById('admin-dashboard');
+    const mainContent = document.getElementById('main-content');
+    const userInfo = document.getElementById('user-info');
+    const userName = document.getElementById('user-name');
+
+
+
 
 })
