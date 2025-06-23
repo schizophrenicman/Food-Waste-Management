c74662d543f45ece0ad4082ba40a944786c9ab19
Shashwat Aryal <shashwataryal@gmail.com>
2025-06-23 14:43:44 +0545

    introduced password validation

--- js/auth.js
+++ js/auth.js
@@ -5,4 +5,36 @@ class AuthManager {
     this.currentUser = null;
     this.currentAdmin = null;
   }
+
+  // Password validation
+  validatePassword(password) {
+    const minLength = 8;
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasNumbers = /\d/.test(password);
+    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+
+    const errors = [];
+    
+    if (password.length < minLength) {
+      errors.push('Password must be at least 8 characters long');
+    }
+    if (!hasUpperCase) {
+      errors.push('Password must contain at least one uppercase letter');
+    }
+    if (!hasLowerCase) {
+      errors.push('Password must contain at least one lowercase letter');
+    }
+    if (!hasNumbers) {
+      errors.push('Password must contain at least one number');
+    }
+    if (!hasSpecialChar) {
+      errors.push('Password must contain at least one special character');
+    }
+
+    return {
+      isValid: errors.length === 0,
+      errors: errors
+    };
+  }
 }
